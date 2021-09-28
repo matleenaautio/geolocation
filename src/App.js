4f72cf58c132cb3213a0f855d53beda0ecd81545
Matleena Autio <c0auma00@students.oamk.fi>
2021-09-28 17:34:36 +0300

    geolocation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [lat, setLat] = useState(0);
+  const [lng, setLng] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        console.log(position);
+        setLat(position.coords.latitude);
+        setLng(position.coords.longitude);
+        setIsLoading(false);
+      }, (error) => {
+        setIsLoading(false);
+        alert(error);
+      })
+    } else {
+      alert("Geolocation not supported!");
+    }
+  }, [])
+
+  if (isLoading) {
+    return <p>Retrieving position..</p>
+  } else {
+    return (
+      <p>
+        Position:
+        {lat.toFixed(3)},
+        {lng.toFixed(3)}
+      </p>
+    );
+  }
+
+  
 }
 
 export default App;
